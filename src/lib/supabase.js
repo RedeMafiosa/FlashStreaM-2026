18a051f3ee7cc7e1aaf228dc2be7bb8fcb45bfe4
RedeMafiosa <jsuispasportugues@gmail.com>
2026-06-14 01:50:31 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://cciufnztjqqumkqebssw.supabase.co/rest/v1/"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjaXVmbnp0anFxdW1rcWVic3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzODIzMTgsImV4cCI6MjA5Njk1ODMxOH0.Z6bSZ7W3uyZ7LVgwt_D25rf_RxA96-t3eDMGuHnez7k"
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
